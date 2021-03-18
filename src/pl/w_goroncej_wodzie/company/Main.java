59f98734da536cd5c470b0e1337afb5be3388fd3
arturOsie <arturosie1@wp.pl>
2021-03-18 22:02:23 +0100

    nazwaprojektu

--- src/pl/w_goroncej_wodzie/company/Main.java
+++ src/pl/w_goroncej_wodzie/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         String version = System.getProperty("java.version");
         System.out.println("Wersja javy: " + version);
+        System.out.println("Nazwa projektu: lab1");
     }
 }
