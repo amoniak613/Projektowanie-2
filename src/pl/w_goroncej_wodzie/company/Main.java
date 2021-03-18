6c02d1a4a2bfc52cf7772f10d79aed6e8e9786cb
Adam <adaajee@wp.pl>
2021-03-18 21:25:04 +0100

    Wersja javy

---
+++ src/pl/w_goroncej_wodzie/company/Main.java
@@ -0,0 +1,9 @@
+package pl.w_goroncej_wodzie.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String version = System.getProperty("java.version");
+        System.out.println("Wersja javy: " + version);
+    }
+}
